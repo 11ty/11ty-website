af99000ec29d9b032061c3be9fcc983263a9fd8a
Zach Leatherman <zachleatherman@gmail.com>
2018-05-21 17:54:59 -0500

    Add more sample sites.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,16 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css/fonts");
 	eleventyConfig.addPassthroughCopy("img");
 
+	eleventyConfig.addFilter("version", function(pkgVersion) {
+		if(pkgVersion.indexOf("file:") === 0) {
+			return "(local)";
+		}
+		if(pkgVersion.indexOf("^") === 0) {
+			return pkgVersion.substr(1);
+		}
+		return pkgVersion;
+	});
+
 	return {
 		templateFormats: ["html", "njk", "md"]
 	};
