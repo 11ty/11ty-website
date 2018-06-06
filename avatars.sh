f2dce4c0c2af130f8c135eb93e7852dabf9f797e
Zach Leatherman <zachleatherman@gmail.com>
2018-06-06 09:29:04 -0500

    Locally host avatars

---
+++ avatars.sh
@@ -0,0 +1,20 @@
+wget https://twitter.com/eduardoboucas/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/eduardoboucas.jpg
+
+wget https://twitter.com/philhawksworth/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/philhawksworth.jpg
+
+wget https://twitter.com/micahmills/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/micahmills.jpg
+
+wget https://twitter.com/hybrid_alex/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/hybrid_alex.jpg
+
+wget https://twitter.com/jameswillweb/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/jameswillweb.jpg
+
+wget https://twitter.com/codypeterson/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/codypeterson.jpg
+
+wget https://twitter.com/wilto/profile_image?size=bigger
+mv profile_image?size=bigger img/avatars/wilto.jpg
