954e54847ef2cb96b7913895bcac362a6fdc1c99
Zach Leatherman <zachleatherman@gmail.com>
2018-06-22 22:44:02 -0500

    Add sessionstorage trick for web fonts.

--- _includes/index.js
+++ _includes/index.js
@@ -1,5 +1,10 @@
 if( "fonts" in document ) {
-	document.fonts.load("1em BenchNine").then(function() {
+	if( sessionStorage.benchnineLoaded ) {
 		document.documentElement.classList.add("benchnine");
-	});
+	} else {
+		document.fonts.load("1em BenchNine").then(function() {
+			sessionStorage.benchnineLoaded = true;
+			document.documentElement.classList.add("benchnine");
+		});
+	}
 }
