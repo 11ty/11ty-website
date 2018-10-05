637ede5b00306954554e5dfb07dcd3fcb76c0169
Zach Leatherman <zachleatherman@gmail.com>
2018-10-05 07:05:46 -0500

    Check for null avatars

--- avatars.sh
+++ avatars.sh
@@ -1,11 +1,21 @@
 for handle in $(cat _data/testimonials.json | jq -r '.[] | .twitter'); do
-  echo ${handle}
-  wget https://twitter.com/${handle}/profile_image?size=bigger
-	mv profile_image?size=bigger img/avatars/${handle}.jpg
+  if [[ "$handle" == "" || "$handle" == "null" ]]
+  then
+    echo "$handle is not valid";
+  else
+    echo ${handle}
+    wget https://twitter.com/${handle}/profile_image?size=bigger
+    mv profile_image?size=bigger img/avatars/${handle}.jpg
+  fi
 done
 
 for handle in $(cat _data/eleventysites.json | jq -r '.[] | .twitter'); do
-  echo ${handle}
-  wget https://twitter.com/${handle}/profile_image?size=bigger
-	mv profile_image?size=bigger img/avatars/${handle}.jpg
+  if [[ "$handle" == "" || "$handle" == "null" ]]
+  then
+    echo "$handle is not valid";
+  else
+    echo ${handle}
+    wget https://twitter.com/${handle}/profile_image?size=bigger
+    mv profile_image?size=bigger img/avatars/${handle}.jpg
+  fi
 done
