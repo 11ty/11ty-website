803b8e113e6e9eb1b65cf0b21e089ef4da71758e
Zach Leatherman <zachleatherman@gmail.com>
2019-01-31 08:48:28 -0600

    Switch to hourly

--- _data/github.js
+++ _data/github.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 function getCacheKey() {
 	let date = new Date();
-	return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
+	return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()} ${date.getUTCHours()}`;
 }
 
 module.exports = async function() {
