34af828c20b6c95bd0d7314321a6a885ee848031
Zach Leatherman <zachleatherman@gmail.com>
2019-01-31 21:06:07 -0600

    Get npm downloads for the last year instead of month.

--- _data/npm.js
+++ _data/npm.js
@@ -8,13 +8,25 @@ function getCacheKey() {
 	return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`;
 }
 
+function pad(num) {
+	return `${num < 10 ? "0" : ""}${num}`;
+}
+function getDateRange(daysOffset) {
+	let date = new Date();
+	if(daysOffset) {
+		date.setTime(date.getTime() + daysOffset*1000*60*60*24);
+	}
+	return `${date.getUTCFullYear()}-${pad(date.getUTCMonth() + 1)}-${pad(date.getUTCDate())}`;
+}
+
 module.exports = async function() {
 	let cache = flatcache.load("npm-downloads", path.resolve("./_datacache"));
 	let key = getCacheKey();
 	let cachedData = cache.getKey(key);
 	if(!cachedData) {
 		console.log( "Fetching new npm download count…" );
-		let newData = await fetch("https://api.npmjs.org/downloads/point/last-month/@11ty/eleventy")
+		// let newData = await fetch("https://api.npmjs.org/downloads/point/last-month/@11ty/eleventy")
+		let newData = await fetch(`https://api.npmjs.org/downloads/point/${getDateRange(-365)}:${getDateRange()}/@11ty/eleventy`)
 			.then(res => res.json())
 			.then(json => {
 				return {
