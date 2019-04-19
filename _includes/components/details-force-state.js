c71dbab64c4b2ef5903820f390712c8eb8cbf628
Zach Leatherman <zachleatherman@gmail.com>
2019-04-18 22:55:11 -0500

    Adds details/summary for menu

---
+++ _includes/components/details-force-state.js
@@ -0,0 +1,24 @@
+if("classList" in document.documentElement) {
+	document.documentElement.classList.add("js");
+}
+
+if( "querySelector" in document ) {
+	var details = document.querySelector("details");
+	var forceOpen = window.getComputedStyle(details).getPropertyValue("--details-force-closed");
+
+	function forceState(isOpen) {
+	  if( isOpen ) {
+	    details.setAttribute("open", "open");
+	  } else {
+	    details.removeAttribute("open");
+	  }
+	}
+
+	if(forceOpen && "matchMedia" in window) {
+	  var mm1 = window.matchMedia(forceOpen);
+	  forceState(!mm1.matches);
+	  mm1.addListener(function(e) {
+	    forceState(!e.matches);
+	  });
+	}
+}
