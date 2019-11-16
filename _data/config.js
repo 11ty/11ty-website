5f10816b72928926835518b231a6c5bc66ddf921
Peter Müller <munter@fumle.dk>
2019-11-16 23:12:23 +0100

    Update github edit link

--- _data/config.js
+++ _data/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	outdated: false,
 	prerelease: false,
 	minifyHtml: true,
-	githubEdit: "https://github.com/11ty/11ty.io/tree/master/",
+	githubEdit: "https://github.com/11ty/11ty.io/blob/master/",
 	now: new Date(),
 	localDev: false
 };
