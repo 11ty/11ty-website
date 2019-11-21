2eb4ed72b32b6a44ff20055f5ea8c2d44620586e
Zach Leatherman <zachleatherman@gmail.com>
2019-11-21 00:06:56 -0600

    node-supporters gets the open collective emails

---
+++ node-supporters.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const fs = require("fs-extra");
+const fetch = require("node-fetch");
+const query = `
+query eleventyMembers {
+  collective(slug: "11ty") {
+    members {
+      nodes {
+        account {
+          id
+          name
+          twitterHandle
+          githubHandle
+          imageUrl
+          memberOf {
+            nodes {
+              account {
+                slug
+              }
+              totalDonations {
+                value
+              }
+            }
+          }
+          ... on Individual {
+            email
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+(async function() {
+  if(process.env.OPENCOLLECT_API_KEY) {
+    let url = "https://api.opencollective.com/graphql/v2";
+    let opts = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Api-Key": process.env.OPENCOLLECT_API_KEY
+      },
+      body: JSON.stringify({ query })
+    };
+
+    let result = await fetch(url, opts)
+      .then(res => res.json())
+      .catch(function(error) {
+        console.error( error );
+      });
+
+    await fs.writeFile("./node-supporters.json", JSON.stringify(result, null, 2));
+    console.log( "Wrote node-supporters.json." );
+  }
+})();
