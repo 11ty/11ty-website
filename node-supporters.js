39ec0e4f4db905eb02d2c0c3d8970795c4813a17
Zach Leatherman <zachleatherman@gmail.com>
2019-11-21 10:02:29 -0600

    Search/button tweaks

--- node-supporters.js
+++ node-supporters.js
@@ -2,27 +2,45 @@ require('dotenv').config();
 
 const fs = require("fs-extra");
 const fetch = require("node-fetch");
+// const query = `
+// query eleventyMembers {
+//   collective(slug: "11ty") {
+//     members {
+//       nodes {
+//         account {
+//           id
+//           name
+//           twitterHandle
+//           githubHandle
+//           imageUrl
+//           memberOf {
+//             nodes {
+//               account {
+//                 slug
+//               }
+//               totalDonations {
+//                 value
+//               }
+//             }
+//           }
+//           ... on Individual {
+//             email
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+// `;
 const query = `
 query eleventyMembers {
   collective(slug: "11ty") {
     members {
       nodes {
         account {
-          id
           name
           twitterHandle
           githubHandle
-          imageUrl
-          memberOf {
-            nodes {
-              account {
-                slug
-              }
-              totalDonations {
-                value
-              }
-            }
-          }
           ... on Individual {
             email
           }
@@ -51,7 +69,24 @@ query eleventyMembers {
         console.error( error );
       });
 
+    //   "data": {
+    // "collective": {
+    //   "members": {
+    //     "nodes": [
+    let emailsOnly = new Set();
+    let members = result.data.collective.members.nodes;
+    for(let member of members) {
+      if(member.account.email) {
+        // TODO check node-supporters/invite.csv for duplicates
+        emailsOnly.add(member.account.email);
+      }
+    }
+
+
     await fs.writeFile("./node-supporters.json", JSON.stringify(result, null, 2));
     console.log( "Wrote node-supporters.json." );
+
+    await fs.writeFile("./node-supporters.csv", Array.from(emailsOnly).join("\n"));
+    console.log( "Wrote node-supporters.csv." );
   }
 })();
