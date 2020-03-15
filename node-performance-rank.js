444fcc607d2e6932f7f6e6d92786037d3f92bd78
Zach Leatherman <zachleatherman@gmail.com>
2020-03-15 13:45:57 -0500

    3 runs for each lighthouse test

--- node-performance-rank.js
+++ node-performance-rank.js
@@ -3,14 +3,49 @@ const fs = require("fs-extra");
 const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
 
-function run(urls = [], opts, config = null) {
+const NUMBER_OF_RUNS = 3;
+
+function mapResultData(result) {
+  if(result.requestedUrl.startsWith("https://github.com/")) {
+    return {
+      url: result.requestedUrl
+    };
+  }
+
+  return {
+    url: result.requestedUrl,
+    finalUrl: result.finalUrl,
+    lighthouseScore: result.categories.performance.score,
+    firstContentfulPaint: result.audits['first-contentful-paint'].numericValue,
+    firstMeaningfulPaint: result.audits['first-meaningful-paint'].numericValue,
+    speedIndex: result.audits['speed-index'].numericValue,
+  };
+}
+
+function sortResultData(a, b) {
+  if(b.lighthouseScore === a.lighthouseScore) {
+    return a.speedIndex - b.speedIndex;
+  }
+  return b.lighthouseScore - a.lighthouseScore
+}
+
+async function runLighthouse(url, opts, config) {
+  let results = [];
+  for(let j = 0; j < NUMBER_OF_RUNS; j++) {
+    results.push(await lighthouse(url, opts, config).then(results => mapResultData(results.lhr)));
+  }
+  results.sort(sortResultData);
+  return results[0];
+}
+
+function run(urls, opts, config = null) {
   return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(async chrome => {
     opts.port = chrome.port;
     let results = [];
     let count = 0;
     for(let url of urls) {
       console.log( `Running (${++count} of ${urls.length}): ${url}` );
-      results.push(await lighthouse(url, opts, config).then(results => results.lhr));
+      results.push(await runLighthouse(url, opts, config));
     }
 
     return chrome.kill().then(() => results);
@@ -34,30 +69,11 @@ const opts = {
     }
   }
 
-  console.log( `Testing ${urls.size} sites:` );
+  let finalUrls = Array.from(urls); //.slice(0, 3);
+  console.log( `Testing ${finalUrls.length} sites:` );
 
-  run(Array.from(urls), opts).then(results => {
-    let finalResults = results.map(result => {
-      // if(siteData.url.startsWith("https://github.com/")) {
-      //   return {
-      //     url: result.requestedUrl
-      //   };
-      // }
-
-      return {
-        url: result.requestedUrl,
-        finalUrl: result.finalUrl,
-        lighthouseScore: result.categories.performance.score,
-        firstContentfulPaint: result.audits['first-contentful-paint'].numericValue,
-        firstMeaningfulPaint: result.audits['first-meaningful-paint'].numericValue,
-        speedIndex: result.audits['speed-index'].numericValue,
-      };
-    }).sort((a, b) => {
-      if(b.lighthouseScore === a.lighthouseScore) {
-        return a.speedIndex - b.speedIndex;
-      }
-      return b.lighthouseScore - a.lighthouseScore
-    });
+  run(finalUrls, opts).then(results => {
+    let finalResults = results.sort(sortResultData);
 
     finalResults = finalResults.map((entry, index) => {
       entry.rank = index + 1;
