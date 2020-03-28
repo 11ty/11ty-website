6663ebb0f088e5bdd70aa0d350c2f1f01a94e4e3
Zach Leatherman <zachleatherman@gmail.com>
2020-03-28 14:39:32 -0500

    Move instant page into script

--- .eleventy.js
+++ .eleventy.js
@@ -112,7 +112,6 @@ ${text.trim()}
 	});
 
 	eleventyConfig.addPassthroughCopy({
-		"node_modules/instant.page/instantpage.js": "js/instant.page.js",
 		"node_modules/@11ty/logo/img/logo.svg": "img/logo.svg",
 		"node_modules/@11ty/logo/img/logo-784x1093.png": "img/logo.png",
 		"node_modules/@11ty/logo/img/logo-300x418.png": "img/logo-github.png",
