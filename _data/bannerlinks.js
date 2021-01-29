201ea1c5592c2799f6ba8230f95cee2ac6b81502
Zach Leatherman <zachleatherman@gmail.com>
2021-01-29 17:07:25 -0600

    New banners only show one cause at a time. Adds Trevor Project to list

---
+++ _data/bannerlinks.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    label: "Black Lives Matter",
+    url: "https://blacklivesmatter.com/",
+  },
+  {
+    label: "The Equal Justice Initiative",
+    url: "https://eji.org/",
+  },
+  {
+    label: "We The Protesters",
+    url: "http://www.wetheprotesters.org/",
+  },
+  {
+    label: "How to permanently delete a Facebook account",
+    url: "https://deletefacebook.com/",
+  },
+  {
+    label: "The Trevor Project — Saving Young LGBTQ Lives",
+    url: "https://www.thetrevorproject.org/",
+  }
+];
