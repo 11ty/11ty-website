176c1f5a8252856ca1c87baa5bdf44fa15d692e5
Zach Leatherman <zachleatherman@gmail.com>
2021-03-31 10:24:09 -0500

    Try chaining, using both of them together was too large

---
+++ functions/image.js
@@ -0,0 +1,60 @@
+const { builderFunction } = require("@netlify/functions");
+const eleventyImage = require("@11ty/eleventy-img")
+
+function isFullUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch(e) {
+    // invalid url OR local path
+    return false;
+  }
+}
+
+// Based on https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/13-returning-dynamic-images/functions/return-image.js
+async function handler(event, context) {
+  let { url, width, format } = event.queryStringParameters;
+
+  try {
+    if(!isFullUrl(url)) {
+      throw new Error(`Invalid \`url\`: ${url}`);
+    }
+
+    let metadata = eleventyImage(url, {
+      formats: [format || "jpeg"],
+      widths: [parseInt(width, 10) || 600], // 260-440 in layout
+      dryRun: true,
+      cacheOptions: {
+        dryRun: true,
+      }
+    });
+
+    let sources = metadata[format];
+
+    if(!Array.isArray(sources) || sources.length === 0) {
+      throw new Error(`Invalid \`format\`: ${format}`);
+    }
+
+    let source = sources[0];
+
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": source.soucceType
+      },
+      body: source.buffer.toString('base64'),
+      isBase64Encoded: true
+    }
+  } catch (error) {
+    console.log("Error", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    }
+  }
+}
+
+exports.handler = builderFunction(handler);
--- functions/screenshot.js
+++ functions/screenshot.js
@@ -1,5 +1,4 @@
 const { builderFunction } = require("@netlify/functions");
-const eleventyImage = require("@11ty/eleventy-img")
 const chromium = require('chrome-aws-lambda');
 
 function isFullUrl(url) {
@@ -45,7 +44,7 @@ async function screenshot(url, withJs = true) {
 
 // Based on https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/13-returning-dynamic-images/functions/return-image.js
 async function handler(event, context) {
-  let { url, js, width, format } = event.queryStringParameters;
+  let { url, js } = event.queryStringParameters;
 
   try {
     if(!isFullUrl(url)) {
@@ -53,29 +52,13 @@ async function handler(event, context) {
     }
 
     let buffer = await screenshot(url, js !== "false");
-    let metadata = eleventyImage(buffer, {
-      formats: [format || "jpeg"],
-      widths: [parseInt(width, 10) || 600], // 260-440 in layout
-      dryRun: true,
-      cacheOptions: {
-        dryRun: true,
-      }
-    });
-
-    let sources = metadata[format];
-
-    if(!Array.isArray(sources) || sources.length === 0) {
-      throw new Error(`Invalid \`format\`: ${format}`);
-    }
-
-    let source = sources[0];
 
     return {
       statusCode: 200,
       headers: {
-        "content-type": source.soucceType
+        "content-type": "image/jpeg"
       },
-      body: source.buffer.toString('base64'),
+      body: buffer.toString('base64'),
       isBase64Encoded: true
     }
   } catch (error) {
