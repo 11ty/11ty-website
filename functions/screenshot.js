cdc47e3549a6cdae4e87ff5250041e0c1f3ff4e7
Zach Leatherman <zachleatherman@gmail.com>
2021-03-31 10:15:04 -0500

    Re-adds Eleventy Image

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -1,4 +1,5 @@
 const { builderFunction } = require("@netlify/functions");
+const eleventyImage = require("@11ty/eleventy-img")
 const chromium = require('chrome-aws-lambda');
 
 function isFullUrl(url) {
@@ -15,7 +16,10 @@ async function screenshot(url, withJs = true) {
   const browser = await chromium.puppeteer.launch({
       executablePath: await chromium.executablePath,
       args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
+      defaultViewport: { // Macbook 13″
+        width: 1440,
+        height: 900
+      },
       headless: chromium.headless,
   });
 
@@ -41,7 +45,7 @@ async function screenshot(url, withJs = true) {
 
 // Based on https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/13-returning-dynamic-images/functions/return-image.js
 async function handler(event, context) {
-  let { url, js } = event.queryStringParameters;
+  let { url, js, width, format } = event.queryStringParameters;
 
   try {
     if(!isFullUrl(url)) {
@@ -49,13 +53,29 @@ async function handler(event, context) {
     }
 
     let buffer = await screenshot(url, js !== "false");
+    let metadata = eleventyImage(buffer, {
+      formats: [format || "jpeg"],
+      widths: [width || 600], // 260-440 in layout
+      dryRun: true,
+      cacheOptions: {
+        dryRun: true,
+      }
+    });
+
+    let sources = metadata[format];
+
+    if(!Array.isArray(sources) || sources.length === 0) {
+      throw new Error(`Invalid \`format\`: ${format}`);
+    }
+
+    let source = sources[0];
 
     return {
       statusCode: 200,
       headers: {
-        "content-type": "image/jpeg"
+        "content-type": source.soucceType
       },
-      body: buffer.toString('base64'),
+      body: source.buffer.toString('base64'),
       isBase64Encoded: true
     }
   } catch (error) {
