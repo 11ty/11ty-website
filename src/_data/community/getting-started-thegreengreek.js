7e28b75390709482f6e07404a70b9ec4a7b33c9b
Zach Leatherman <zachleatherman@gmail.com>
2021-07-15 08:11:11 -0500

    Adds new tutorial

---
+++ src/_data/community/getting-started-thegreengreek.js
@@ -0,0 +1,6 @@
+module.exports = {
+  url: "https://sia.codes/posts/itsiest-bitsiest-eleventy-tutorial/",
+  author: "thegreengreek",
+  title: "Itsiest, Bitsiest Eleventy Tutorial",
+  key: "getting-started",
+};
