48438cc62d2c264b096591957450daaf3bed2bf9
Zach Leatherman <zachleatherman@gmail.com>
2021-07-15 11:41:56 -0500

    Add Uniclode

---
+++ src/_data/demos/uniclode.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "Uniclode",
+	category: ["serverless"],
+	url: "https://uniclode.netlify.app/",
+	source_url: "https://github.com/zachleat/uniclode",
+	twitter: "zachleat",
+};
