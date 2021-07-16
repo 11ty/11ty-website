3d1542aa033dcfcf802786ca10a5e8f666f3f236
Tanner <tannercdolby@gmail.com>
2021-07-16 02:18:19 -0700

    Add community post for global data

---
+++ src/_data/community/global-data-tannerdolby.js
@@ -0,0 +1,6 @@
+module.exports = {
+	url: "https://tannerdolby.com/writing/generate-page-content-from-a-global-data-file-using-eleventy/",
+	author: "tannerdolby",
+	title: "Generate Page Content from a Global Data File",
+	key: "global-data"
+};
