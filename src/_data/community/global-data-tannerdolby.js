531943e8aa56ce8c5a2a3ab13a7fb001e9fdd1a0
Tanner <tannercdolby@gmail.com>
2021-07-16 02:21:44 -0700

    Use the correct value for key

--- src/_data/community/global-data-tannerdolby.js
+++ src/_data/community/global-data-tannerdolby.js
@@ -2,5 +2,5 @@ module.exports = {
 	url: "https://tannerdolby.com/writing/generate-page-content-from-a-global-data-file-using-eleventy/",
 	author: "tannerdolby",
 	title: "Generate Page Content from a Global Data File",
-	key: "global-data"
+	key: "data"
 };
