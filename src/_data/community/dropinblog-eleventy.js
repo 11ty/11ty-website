95712a3f2ccd05da459f18498198449501a1feca
David Darnes <me@daviddarnes.com>
2021-10-13 14:17:24 +0100

    Add resource for using DropInBlog with Eleventy

---
+++ src/_data/community/dropinblog-eleventy.js
@@ -0,0 +1,6 @@
+module.exports = {
+  url: "https://dropinblog.com/blog/blogging-on-the-jamstack-with-netlify-and-eleventy/",
+  author: "daviddarnes",
+  title: "Blogging on the Jamstack with DropInBlog, Netlify and Eleventy",
+  key: "data",
+};
