ee5f865274dec5bac866d130f4422a39c27f2d15
Zach Leatherman <zachleatherman@gmail.com>
2021-10-26 10:44:24 -0500

    Switch to global persistent builders via https://www.netlify.com/blog/2021/10/25/faster-more-reliable-page-loads-with-update-to-on-demand-builders/ via Eleventy 1.0.0-beta.3

--- .eleventy.js
+++ .eleventy.js
@@ -148,6 +148,7 @@ module.exports = function(eleventyConfig) {
 		name: "serverless",
 		inputDir: "src",
 		functionsDir: "./netlify/functions/",
+		redirects: "netlify-toml-builders",
 		// copyEnabled: process.env.NODE_ENV === "production",
 		copy: [
 			"config/",
@@ -164,6 +165,7 @@ module.exports = function(eleventyConfig) {
 		name: "dynamic",
 		inputDir: "src",
 		functionsDir: "./netlify/functions/",
+		redirects: "netlify-toml-functions",
 		// copyEnabled: process.env.NODE_ENV === "production",
 		copy: [
 			"config/",
