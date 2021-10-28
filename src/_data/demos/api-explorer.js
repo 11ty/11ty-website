4f21ee21f86dbccfaee5cbb9912d9fa87bedd5cc
Zach Leatherman <zachleatherman@gmail.com>
2021-10-28 10:00:12 -0500

    Adds API explorer to serverless demos

---
+++ src/_data/demos/api-explorer.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "Eleventy API Explorer",
+	category: ["serverless"],
+	url: "https://api-explorer.11ty.dev/",
+	source_url: "https://github.com/11ty/api-explorer/",
+	twitter: "zachleat",
+}
+
