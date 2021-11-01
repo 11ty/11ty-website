f26162adc4e22753bbe33f852ae15be70bc34ded
Peter deHaan <pdehaan@hey.com>
2021-11-01 12:13:40 -0700

    Create image-hovhadovah.js (#1215)

---
+++ src/_data/community/image-hovhadovah.js
@@ -0,0 +1,6 @@
+module.exports = {
+	url: "https://www.aleksandrhovhannisyan.com/blog/eleventy-image-lazy-loading/",
+	author: "hovhaDovah",
+	title: "Lazily Loading Images with the 11ty Image Plugin",
+	key: "image"
+};
