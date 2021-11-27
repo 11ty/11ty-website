8497972e70259e4f3351fcf8aec64f7b2a5005a3
Dana Byerly <dana@superterrific.org>
2021-11-27 14:58:33 -0500

    Adding community resources
    Adding three articles from my site, not sure I used the right key. Thank you!

---
+++ src/_data/community/quicktips-ghostmode-superterrific.js
@@ -0,0 +1,6 @@
+module.exports = {
+	url: "https://danabyerly.com/notes/how-to-turn-off-browsersync-ghostmode-in-eleventy/",
+	author: "superterrific",
+	title: "How to turn off Browsersync ghostMode in Eleventy",
+	key: "quicktips",
+};
