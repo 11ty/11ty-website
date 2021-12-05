df0e824abe6994f9ffd25b8d4a049c8749a7f923
Dana Byerly <34495737+superterrific@users.noreply.github.com>
2021-12-05 13:44:17 -0500

    Adding community resources (#1236)

---
+++ src/_data/community/quicktips-headinglevels-superterrific.js
@@ -0,0 +1,6 @@
+module.exports = {
+	url: "https://danabyerly.com/articles/dynamic-heading-levels-in-eleventy/",
+	author: "superterrific",
+	title: "Dynamic heading levels in Eleventy",
+	key: "quicktips",
+};
