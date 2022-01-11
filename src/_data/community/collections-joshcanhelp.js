be5097332b0cf64b3fc4bc2b8671589846ee1a4d
Josh Cunningham <josh@joshcanhelp.com>
2022-01-10 17:57:38 -0800

    Add a community Collection link

---
+++ src/_data/community/collections-joshcanhelp.js
@@ -0,0 +1,6 @@
+module.exports = {
+	url: "https://www.joshcanhelp.com/eleventy-custom-content-type-collections/",
+	author: "joshcanhelp",
+	title: "Custom Content Collections",
+	key: "collections",
+};
