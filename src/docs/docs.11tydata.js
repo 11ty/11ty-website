5403f2b853e09165bec8bc6f7466a6a041487bcc
Zach Leatherman <zachleatherman@gmail.com>
2022-02-18 10:51:00 -0600

    Limit git last modified dates to production environment only

--- src/docs/docs.11tydata.js
+++ src/docs/docs.11tydata.js
@@ -1,10 +1,9 @@
 let data = {
 	layout: "layouts/docs.njk",
-	date: "git Last Modified",
 };
-// Use this if the git thing is too slow
-// if(process.env.NODE_ENV === "production") {
-// 	data.date = "git Last Modified";
-// }
+
+if(process.env.NODE_ENV === "production") {
+	data.date = "git Last Modified";
+}
 
 module.exports = data;
