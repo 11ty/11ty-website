6c3319dddca73a69c40a9e90b90ad4350cbf5cb6
Zach Leatherman <zachleatherman@gmail.com>
2022-02-18 20:49:02 -0600

    Fixes #1337

--- config/addedin.js
+++ config/addedin.js
@@ -1,6 +1,8 @@
 const semver = require("semver");
 const versions = require("../src/_data/versions");
 
+const MINIMUM_VERSION_SHOWN = "0.11.0";
+
 module.exports = eleventyConfig => {
 	eleventyConfig.addShortcode("addedin", function(version, tag, extraClass) {
 		const newestPublishedVersion = versions.filter(v => v.tag !== "LATEST").shift();
@@ -16,6 +18,10 @@ module.exports = eleventyConfig => {
 			// only works for versions starting with a number (plugins don’t do this)
 			// is the latest version less than or equal to the version being passed in here?
 			hasBeenReleased = semver.lte(version, semver.coerce(newestPublishedVersion.tag));
+
+			if(semver.lt(version, MINIMUM_VERSION_SHOWN)) {
+				return "";
+			}
 		}
 
 		tag = tag || "span";
