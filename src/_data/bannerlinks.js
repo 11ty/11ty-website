e21ee76ed93bc12d319165d04e07bd01360e66b1
Zach Leatherman <zachleatherman@gmail.com>
2022-02-24 09:14:54 -0600

    Stand with Ukraine

--- src/_data/bannerlinks.js
+++ src/_data/bannerlinks.js
@@ -1,26 +1,30 @@
 module.exports = [
+  // {
+  //   label: "Black Lives Matter",
+  //   url: "https://blacklivesmatter.com/",
+  // },
+  // {
+  //   label: "The Equal Justice Initiative",
+  //   url: "https://eji.org/",
+  // },
+  // {
+  //   label: "We The Protesters",
+  //   url: "http://www.wetheprotesters.org/",
+  // },
+  // {
+  //   label: "How to permanently delete a Facebook account",
+  //   url: "https://deletefacebook.com/",
+  // },
+  // {
+  //   label: "The Trevor Project — Saving Young LGBTQ Lives",
+  //   url: "https://www.thetrevorproject.org/",
+  // },
+  // {
+  //   label: "Asian Americans Advancing Justice — Stand Against Hatred",
+  //   url: "https://www.standagainsthatred.org/"
+  // },
   {
-    label: "Black Lives Matter",
-    url: "https://blacklivesmatter.com/",
-  },
-  {
-    label: "The Equal Justice Initiative",
-    url: "https://eji.org/",
-  },
-  {
-    label: "We The Protesters",
-    url: "http://www.wetheprotesters.org/",
-  },
-  {
-    label: "How to permanently delete a Facebook account",
-    url: "https://deletefacebook.com/",
-  },
-  {
-    label: "The Trevor Project — Saving Young LGBTQ Lives",
-    url: "https://www.thetrevorproject.org/",
-  },
-  {
-    label: "Asian Americans Advancing Justice — Stand Against Hatred",
-    url: "https://www.standagainsthatred.org/"
+    label: "Stand With Ukraine 🇺🇦",
+    url: "https://twitter.com/Ukraine"
   }
 ];
