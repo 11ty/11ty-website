1dc4b804419a0438d2305dc3ca3862243cecbe48
Zach Leatherman <zachleatherman@gmail.com>
2022-04-29 09:58:20 -0500

    Remove HttpOnly cookie for user preferences, so that we can use them in JS later (maybe?)

--- netlify/edge-functions/user-preferences.js
+++ netlify/edge-functions/user-preferences.js
@@ -1,33 +1,34 @@
 function setCookie(context, name, value) {
-  context.cookies.set({
-    name,
-    value,
-    path: "/",
-    httpOnly: true,
-    secure: true,
-    sameSite: "Lax",
-  });
+	context.cookies.set({
+		name,
+		value,
+		path: "/",
+		// want this in JS too, maybe for later
+		// httpOnly: true,
+		secure: true,
+		sameSite: "Lax",
+	});
 }
 
 export default async (request, context) => {
-  let url = new URL(request.url);
+	let url = new URL(request.url);
 
-  // Save to cookie, redirect back to form
-  if(url.pathname === "/account/preferences/" && request.method === "POST") {
-    if(request.headers.get("content-type") === "application/x-www-form-urlencoded") {
-      let body = await request.text();
-      let postData = Object.fromEntries(new URLSearchParams(body));
-      // setCookie(context, "appearance", postData.appearance);
-      setCookie(context, "syntax", postData.syntax);
+	// Save to cookie, redirect back to form
+	if(url.pathname === "/account/preferences/" && request.method === "POST") {
+		if(request.headers.get("content-type") === "application/x-www-form-urlencoded") {
+			let body = await request.text();
+			let postData = Object.fromEntries(new URLSearchParams(body));
+			// setCookie(context, "appearance", postData.appearance);
+			setCookie(context, "syntax", postData.syntax);
 
-      return new Response(null, {
-        status: 302,
-        headers: {
-          location: postData.returnlocation || "/docs/"
-        }
-      });
-    }
-  }
+			return new Response(null, {
+				status: 302,
+				headers: {
+					location: postData.returnlocation || "/docs/"
+				}
+			});
+		}
+	}
 
-  return context.next();
+	return context.next();
 };
