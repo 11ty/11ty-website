1a17d544cf176a57e6280caf6418b55c18d22131
Zach Leatherman <zachleatherman@gmail.com>
2022-08-01 14:27:04 -0500

    Switch to use :defined instead of .js

--- src/_includes/components/js-class.js
+++
@@ -1,8 +0,0 @@
-if("classList" in document.documentElement) {
-	var docEl = document.documentElement;
-	docEl.classList.add("js");
-
-	if ("open" in document.createElement("details")) {
-		docEl.classList.add("supports-detailssummary");
-	}
-}
