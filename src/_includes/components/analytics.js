6b320c15e6bdae5428679d0d1ba4c3a5dd74217f
Zach Leatherman <zachleatherman@gmail.com>
2023-10-01 15:44:37 -0500

    Revert

--- src/_includes/components/analytics.js
+++
@@ -1,6 +0,0 @@
-// home-made analytics 😅
-if(location.hostname === "www.11ty.dev") {
-	navigator.sendBeacon("https://subtle-yeot-bd8178.netlify.app/", JSON.stringify({
-		path: location.pathname
-	}));
-}
