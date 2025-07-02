b85df6096c1e2416128adbaa1fc8176256622711
Zach Leatherman <zachleat@users.noreply.github.com>
2025-07-02 16:58:06 -0500

    When after event isn’t used, output dir needs to be created

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 
+import { mkdirSync } from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import memoize from "memoize";
@@ -457,6 +458,9 @@ ${text.trim()}
 
 	// Eleventy Editor
 	// Minification only happens in production
+	mkdirSync(eleventyConfig.directories.output, {
+		recursive: true
+	});
 	await minifyJavaScriptFile(resolveModule("@11ty/eleventy/bundle"), path.join(eleventyConfig.directories.output, "js/eleventy.core.browser.js"));
 	await minifyJavaScriptFile(resolveModule("@11ty/eleventy/bundle/md"), path.join(eleventyConfig.directories.output, "js/eleventy.engine-md.browser.js"));
 	await minifyJavaScriptFile(resolveModule("@11ty/eleventy/bundle/liquid"), path.join(eleventyConfig.directories.output, "js/eleventy.engine-liquid.browser.js"));
