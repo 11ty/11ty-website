9906b3e03c2b21f6e8e01d50e3e515fc696c76f2
Tom MacWright <tom@macwright.com>
2026-02-16 18:18:29 -0500

    [WIP] floating right menu

--- src/docs/docs.11tydata.js
+++ src/docs/docs.11tydata.js
@@ -1,8 +1,16 @@
+import { createToc } from "../../config/toc.js";
+
 let data = {
 	layout: "layouts/docs.njk",
 	headerTitle: "Eleventy Documentation",
 	feedTitle: "Eleventy Documentation",
 	feedUrl: "/docs/feed.xml",
+	eleventyComputed: {
+		tocData: (data) => {
+			const toc = createToc(data.page.rawInput, {});
+			return toc;
+		},
+	},
 };
 
 if (process.env.NODE_ENV === "production") {
