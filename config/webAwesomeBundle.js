41904516958faed06ccee275d9ec3c603fd353a2
Zach Leatherman <zachleat@users.noreply.github.com>
2026-03-04 16:56:09 -0600

    Adds new home page treatment without comparison component

--- config/webAwesomeBundle.js
+++ config/webAwesomeBundle.js
@@ -1,4 +1,2 @@
 // <wa-copy-button>
 import "@awesome.me/webawesome/dist/components/copy-button/copy-button.js";
-// <wa-comparison>
-import "@awesome.me/webawesome/dist/components/comparison/comparison.js";
