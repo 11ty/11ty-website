da1cfeceb97e8d62b3d81017a0afdccfac5bc61a
Zach Leatherman <zachleat@users.noreply.github.com>
2026-03-04 11:22:26 -0600

    Status update

--- config/webAwesomeBundle.js
+++ config/webAwesomeBundle.js
@@ -2,3 +2,9 @@
 import "@awesome.me/webawesome/dist/components/copy-button/copy-button.js";
 // <wa-comparison>
 import "@awesome.me/webawesome/dist/components/comparison/comparison.js";
+
+// <wa-dialog>
+import "@awesome.me/webawesome/dist/components/dialog/dialog.js";
+import "@awesome.me/webawesome/dist/components/button/button.js";
+import "@awesome.me/webawesome/dist/components/icon/icon.js";
+import "@awesome.me/webawesome/dist/components/spinner/spinner.js";
