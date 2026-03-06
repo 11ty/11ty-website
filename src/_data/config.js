eca66927286848cb272d882c681282ebc52b8d42
Zach Leatherman <zachleat@users.noreply.github.com>
2026-03-06 11:13:12 -0600

    Update Kickstarter URL

--- src/_data/config.js
+++ src/_data/config.js
@@ -5,5 +5,5 @@ export default {
 	now: new Date(),
 	env: process.env.NODE_ENV,
 	origin: "https://www.11ty.dev", // important: no trailing slash on URL
-	kickstarterUrl: "https://www.kickstarter.com/projects/fontawesome/build-awesome?ref=2qzv71",
+	kickstarterUrl: "https://www.kickstarter.com/projects/fontawesome/build-awesome-pro?ref=43ttgb",
 };
