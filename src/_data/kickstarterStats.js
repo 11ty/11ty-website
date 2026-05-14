1511831b244f312041cc5cec6d93d0374f58417f
Zach Leatherman <zachleat@users.noreply.github.com>
2026-05-14 15:28:58 -0500

    Remove superfluous comment

--- src/_data/kickstarterStats.js
+++ src/_data/kickstarterStats.js
@@ -1,8 +1,5 @@
 import Fetch from "@11ty/eleventy-fetch";
 
-// _data/campaign.js — drop this in your Eleventy _data/ folder
-// Eleventy will call this at build time and expose the result as {{ campaign }}
-
 export default async function () {
   const url = "https://11ty.github.io/kickstarter-api/build-awesome-pro.json";
   return Fetch(url, {
