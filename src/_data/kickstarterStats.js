7f2935373029d3644730df3aaf61d453a85ec55b
Zach Leatherman <zachleat@users.noreply.github.com>
2026-05-28 09:02:02 -0500

    Change cache duration for kickstarter stats

--- src/_data/kickstarterStats.js
+++ src/_data/kickstarterStats.js
@@ -4,6 +4,6 @@ export default async function () {
   const url = "https://11ty.github.io/kickstarter-api/build-awesome-pro.json";
   return Fetch(url, {
 		type: "json",
-		duration: "30m",
+		duration: "15m",
 	});
 }
