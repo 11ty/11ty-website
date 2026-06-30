79fcc4fc72cb8c0849eb0beacfeeb1f1abdf2e91
Zach Leatherman <zachleat@users.noreply.github.com>
2026-06-30 11:02:03 -0500

    Change github release cache duration from 24h to 6h

--- src/_data/github.js
+++ src/_data/github.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import Fetch from "@11ty/eleventy-fetch";
 
-const CACHE_DURATION = process.env.ELEVENTY_RUN_MODE === "serve" ? "7d" : "1d";
+const CACHE_DURATION = process.env.ELEVENTY_RUN_MODE === "serve" ? "7d" : "6h";
 
 export default async function () {
 	try {
